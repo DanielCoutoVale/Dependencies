9e0d5e6275462f75fd2a5cbc0f0a7e0ccbf0d37c
Daniel Couto-Vale <danielvale@icloud.com>
2020-07-07 22:25:21 +0200

    Add script to import UD files

--- src/org/dependencies/ImportUdFile.java
+++ src/org/dependencies/ImportUdFile.java
@@ -3,34 +3,39 @@ package org.dependencies;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import org.dependencies.sys.SysFileImporter;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.dependencies.ud.UdFileImporter;
+import org.xml.sax.SAXException;
 
 /**
- * Imports a SYS file.
+ * Imports a UD file.
  * 
  * @author Daniel Couto-Vale
  */
-public class ImportSysFile {
-	
+public class ImportUdFile {
+
 	/**
 	 * Main
 	 * 
 	 * @param args the script arguments
 	 */
-	public final static void main(String[] args) {
+	public static final void main(String[] args) {
 		if (args.length != 2) {
 			System.err.println("USAGE:");
-			System.err.println("java -jar import-sys-file.jar [description] [file]");
+			System.err.println("java -jar import-ud-file.jar [description] [file]");
 		}
+		UdFileImporter importer = new UdFileImporter();
 		String descriptionName = args[0];
 		String fileName = args[1];
-		SysFileImporter importer = new SysFileImporter();
 		try {
 			importer.importFile(descriptionName, fileName);
-		} catch (IOException | SQLException e) {
+		} catch (ParserConfigurationException | SAXException | IOException | SQLException e) {
 			e.printStackTrace();
 			System.err.println("There is a bug in this script.");
 		}
 	}
+
 	
+
 }
---
+++ src/org/dependencies/ud/UdFileImporter.java
@@ -0,0 +1,57 @@
+package org.dependencies.ud;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.dependencies.base.MysqlDependencyBase;
+import org.dependencies.model.DepDescription;
+import org.dependencies.model.DepFeature;
+import org.dependencies.model.DepFunction;
+import org.dependencies.model.DepMetafunction;
+import org.dependencies.model.DepSystem;
+import org.xml.sax.SAXException;
+
+/**
+ * A UD file importer.
+ * 
+ * @author Daniel Couto-Vale
+ */
+public class UdFileImporter {
+
+	public final void importFile(String descriptionName, String fileName) throws ParserConfigurationException, SAXException, IOException, SQLException {
+		File file = new File(fileName);
+		UdDocumentBuilder builder = new UdDocumentBuilder();
+		UdDocument document = builder.parse(file);
+		MysqlDependencyBase base = new MysqlDependencyBase();
+		DepDescription description = base.addDescription(descriptionName);
+		DepMetafunction metafunction = base.addMetafunction(description.getId(), "mixed");
+		DepSystem rankSystem = base.addSystem(description.getId(), "RANK", "wording");
+		DepFeature rankFeature = base.addFeature(rankSystem.getId(), "word");
+		rankSystem.addFeature(rankFeature);
+		DepSystem tagSystem = base.addSystem(description.getId(), "WORD-CLASS", "word");
+		description.addSystem(tagSystem);
+		for (UdTag tag : document.getTags()) {
+			DepFeature feature = base.addFeature(tagSystem.getId(), tag.getName());
+			tagSystem.addFeature(feature);
+		}
+		for (UdFeature feat : document.getFeats()) {
+			DepSystem system = description.getSystem(feat.getName());
+			if (system == null) {
+				String entryCondition = String.join(" ∨ ", feat.getTagNames());
+				system = base.addSystem(description.getId(), feat.getName(), entryCondition);
+				description.addSystem(system);
+			}
+			DepFeature feature = base.addFeature(system.getId(), feat.getValue());
+			tagSystem.addFeature(feature);
+		}
+		for (UdDependency dep : document.getDeps()) {
+			DepFunction function = base.addFunction(metafunction.getId(), dep.getName());
+			description.addFunction(function);
+		}
+		base.close();
+	}
+	
+}
