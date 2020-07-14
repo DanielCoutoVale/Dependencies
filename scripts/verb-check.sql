1f4b45e5f3827cd5f4dffa4974d363576fa350bc
Daniel Couto-Vale <danielvale@icloud.com>
2020-07-14 12:30:45 +0200

    Implement Step 1 roughly

--- scripts/verb-check.sql
+++ scripts/verb-check.sql
@@ -4,12 +4,12 @@ GROUP BY `class`;#1318+4663
 
 SELECT count(`id`), `class` FROM dependencies.`ip-word`
 WHERE `class` = 'verb'
-GROUP BY `class`;#1511
+GROUP BY `class`;#5960
 
 SELECT count(`id1`) AS `a-frequency`, (100 * count(`id1`)) / (1318+4663) AS `r-frequency`, `class1`, `class2`, `function` FROM `ittb-anchor`
 WHERE `class1` = 'aux' OR `class1` = 'verb'
 GROUP BY `class1`, `class2`, `function`;
 
-SELECT count(`id1`) AS `a-frequency`, (100 * count(`id1`)) / 1511 AS `r-frequency`, `class1`, `class2`, `function` FROM `ip-anchor`
+SELECT count(`id1`) AS `a-frequency`, (100 * count(`id1`)) / 5960 AS `r-frequency`, `class1`, `class2`, `function` FROM `ip-anchor`
 WHERE `class1` = 'verb'
 GROUP BY `class1`, `class2`, `function`;
